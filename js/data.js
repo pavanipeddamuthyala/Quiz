07fe2dea642b01b8bb1e22ee126903725cf9edbe
pavanipeddamuthyala <67213893+pavanipeddamuthyala@users.noreply.github.com>
2021-02-14 10:29:19 +0530

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,72 @@
+var jsonData = [
+    {
+        "q" : "The common element which describe the web page, is ?",
+        "opt1" : "heading",
+        "opt2" : "paragraph",
+        "opt3" : "All of these",
+        "answer" : "All of these"
+    },
+    {
+        "q" : "HTML stands for?",
+        "opt1" : "Hyper Text Markup Language",
+        "opt2" : "High Text Markup Language",
+        "opt3" : "Hyper Tabular Markup Language",
+        "answer" : "Hyper Text Markup Language"
+    },
+    {
+        "q" : "which of the following tag is used to mark a begining of paragraph ?",
+        "opt1" : "TD",
+        "opt2" : "br",
+        "opt3" : "P",
+        "answer" : "P"
+    },
+    {
+        "q" : "From which tag descriptive list starts ?",
+        "opt1" : "LL",
+        "opt2" : "DL",
+        "opt3" : "DD",
+        "answer" : "DL"
+    },
+    {
+        "q" : "Correct HTML tag for the largest heading is _____",
+        "opt1" : "h1",
+        "opt2" : "h6",
+        "opt3" : "heading",
+        "answer" : "h1"
+    },
+    {
+         "q" : "The tags in HTML are ?",
+         "opt1" : "case-sensitive",
+         "opt2" : "in upper case",
+         "opt3" :  "not case sensitive",
+         "answer"  : "not case sensitive"
+   },
+   {
+          "q" : "<input> is ?",
+          "opt1" : "a format tag",
+          "opt2" : "an empty tag",
+          "opt3" : "all the above",
+          "answer" : "an empty tag" 
+  },
+  {
+        "q" : "HTML tags are enclosed in ?",
+        "opt1" : "# and #",
+        "opt2" : "{ and }", 
+        "opt3" : "< and >",
+        "answer" : "< and >"
+ },
+ {
+       "q" : "which provides unique name to element ?",
+       "opt1" : "class",
+       "opt2" : "id",
+       "opt3" : "type",
+       "answer" : "id"
+ },
+ {
+      "q" : "A program in HTML can be rendered and read by ?",
+      "opt1" : "web browser",
+      "opt2" : "server",
+      "opt3" : "interpreter",
+      "answer" : "web browser"
+ }
+];
